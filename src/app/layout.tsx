e95c56462b545653eab77b9735defaced64c993b
sabikuntazina2025 <sabikuntazina@gmail.com>
2026-08-09 01:03:12 +0600

    all setup done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ToastContainer } from "react-toastify";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,7 +24,16 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <main>
+          {children}
+        </main>
+        
+        <ToastContainer
+          position="top-right"
+          autoClose={3000}
+        />
+      </body>
     </html>
   );
 }
